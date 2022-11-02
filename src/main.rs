4711dc511534db2a93ab27520138bc37f2dfef67
TheEmeraldBee <brightonlcox@gmail.com>
2022-11-01 19:19:11 -0600

    Greatest Common Denominator Test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+
+    n
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n = 17;
+    let m = 293;
+    let x = gcd(n, m);
+    println!("The GCD of {n} and {m} is {x}");
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(100, 500), 100);
+    //TODO: Fix
+    //assert_eq!(gcd(0, 0));
 }
