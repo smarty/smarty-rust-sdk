9551b41c451be97932b1397010e6f4c31d44ac5c
Brighton Cox <brightonlcox@gmail.com>
2025-06-13 22:25:22 -0600

    Added etag to us_enrichment_api Unable to test due to not having access to apis.

--- smarty-rust-sdk/src/us_enrichment_api/results.rs
+++ smarty-rust-sdk/src/us_enrichment_api/results.rs
@@ -1,9 +1,11 @@
+use serde::de::DeserializeOwned;
 use serde::Deserialize;
 use serde::Serialize;
 use std::fmt::Debug;
-use serde::de::DeserializeOwned;
+use std::ops::Deref;
+use std::ops::DerefMut;
 
-pub trait EnrichmentResponse: Clone + Serialize + DeserializeOwned {
+pub trait EnrichmentResponse: Clone + Serialize + DeserializeOwned + Default {
     fn lookup_type() -> &'static str;
 }
 
@@ -550,5 +552,126 @@ pub struct FinancialResponse {
 impl EnrichmentResponse for FinancialResponse {
     fn lookup_type() -> &'static str {
         "financial"
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct GeoReferenceResponse {
+    pub census_block: CensusBlock,
+    pub census_county_division: CensusCountyDivision,
+    pub census_tract: CensusTract,
+    pub core_based_stat_area: CoreBasedStatArea,
+    pub place: Place,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct CensusBlock {
+    pub accuracy: CensusGeoIdAccuracy,
+    pub geoid: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum CensusGeoIdAccuracy {
+    #[default]
+    #[serde(rename = "block")]
+    Block,
+    #[serde(rename = "tract")]
+    Tract,
+    #[serde(rename = "county")]
+    County,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum GeocodeAccuracy {
+    #[default]
+    #[serde(rename = "inferred")]
+    Inferred,
+    #[serde(rename = "exact")]
+    Exact,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct CensusCountyDivision {
+    accuracy: GeocodeAccuracy,
+    code: String,
+    name: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct CensusTract {
+    code: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct CoreBasedStatArea {
+    code: String,
+    name: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Place {
+    accuracy: GeocodeAccuracy,
+    code: String,
+    name: String,
+    #[serde(rename = "type")]
+    type_: PlaceType,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum PlaceType {
+    #[default]
+    #[serde(rename = "unincorporated")]
+    Unincorperated,
+    #[serde(rename = "incorperated")]
+    Incorperated,
+}
+
+impl EnrichmentResponse for GeoReferenceResponse {
+    fn lookup_type() -> &'static str {
+        "geo-reference"
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct GeoReference2010Response(pub GeoReferenceResponse);
+
+impl Deref for GeoReference2010Response {
+    type Target = GeoReferenceResponse;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for GeoReference2010Response {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl EnrichmentResponse for GeoReference2010Response {
+    fn lookup_type() -> &'static str {
+        "geo-reference/2010"
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct GeoReference2020Response(pub GeoReferenceResponse);
+
+impl Deref for GeoReference2020Response {
+    type Target = GeoReferenceResponse;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for GeoReference2020Response {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl EnrichmentResponse for GeoReference2020Response {
+    fn lookup_type() -> &'static str {
+        "geo-reference/2020"
     }
 }
